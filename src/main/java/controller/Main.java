ed18449c1634f5dbe030f90734cf43760137f527
Darkam640 <josuenoxd@gmail.com>
2024-05-30 04:56:33 -0600

    commit para hacer merge en main

--- src/main/java/controller/Main.java
+++ src/main/java/controller/Main.java
@@ -8,15 +8,15 @@ import view.FRM_Menu;
 public class Main {
 
     public static void main(String[] args) {
-        // Crear instancias de FRM_Client y FRM_Menu
+
         FRM_Client frmClient = new FRM_Client();
         FRM_Menu frmMenu = new FRM_Menu();
 
-        // Crear instancias de MenuManager y ReservationManager
+
         MenuManager menuManager = new MenuManager(frmMenu);
         ReservationManager reservationManager = new ReservationManager(frmClient, menuManager);
 
-        // Mostrar el formulario de clientes
+
         frmMenu.setVisible(true);
     }
 }
